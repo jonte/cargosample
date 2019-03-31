4eb6df4532a5b17da0e745855341b20fbeef12ad
Jonatan Pålsson <jonatan.p@gmail.com>
2019-03-31 12:35:23 +0200

    Generalize to support more packages

--- get_cargo_deps.py
+++ get_cargo_deps.py
@@ -17,6 +17,8 @@
 import subprocess, sys, os
 import pathlib
 
+cargo_url_templ = "https://crates.io/api/v1/crates/%s/%s/download"
+
 header_templ = '''project('%s', 'rust', version : '%s')
 
 '''
@@ -29,25 +31,31 @@ target_templ = '''%s_lib = static_library('%s',
 dep_templ = '''%s_dep = declare_dependency(link_with : %s_lib)\n'''
 
 def get_crate_repo_url(crate_name, crate_version):
-    # Crates.io does not seem to provide a query API
-    # and the web site is 100% javascript so web scraping
-    # is impossible. Thanks.
-    if crate_name != 'itoa':
-        sys.exit('This demo script only supports the itoa crate.')
-    if crate_version != '0.3.4':
-        sys.exit('This demo script only supports version 0.3.4.')
-    return 'https://github.com/dtolnay/itoa.git'
+    return cargo_url_templ % (crate_name, crate_version)
 
-def create_checkout(crate_name, crate_version):
-    spdir = pathlib.Path('subprojects')
-    outdir = spdir / ('%s-%s' % (crate_name, crate_version))
+def unpack_crate(spdir, crate_file_path, crate_dir_name):
+    outdir = spdir / crate_dir_name
     if outdir.exists():
         print('Output path %s already exists, not doing a checkout.' % outdir)
         return outdir
-    crate_url = get_crate_repo_url(crate_name, crate_version)
-    subprocess.check_call(['git', 'clone', crate_url, str(outdir)])
-    subprocess.check_call(['git', 'checkout', crate_version], cwd=outdir)
+
+    subprocess.check_call(['tar', 'xf', crate_file_path,
+        '--one-top-level=%s' % crate_dir_name], cwd=spdir)
+
     return outdir
+
+def create_checkout(crate_name, crate_version):
+    spdir = pathlib.Path('subprojects')
+    crates_dir = spdir / 'crates'
+    crate_dir_name = '%s-%s' % (crate_name, crate_version)
+    crate_file_path = (crates_dir / (crate_dir_name + '.crate')).resolve()
+    crate_url = get_crate_repo_url(crate_name, crate_version)
+
+    crates_dir.mkdir(exist_ok = True)
+    subprocess.check_call(['curl', '-sL', crate_url, '-o', crate_file_path])
+
+    return unpack_crate(spdir, crate_file_path, crate_dir_name)
+
 
 def create_mesonfiles(crate_name, crate_version, outdir):
     mfile = outdir / 'meson.build'
--- prog.rs
+++ prog.rs
@@ -1,5 +1,17 @@
 extern crate itoa;
 
+#[macro_use]
+extern crate bitflags;
+
+bitflags! {
+    struct Flags: u32 {
+        const A = 0b00000001;
+        const B = 0b00000010;
+        const C = 0b00000100;
+        const ABC = Self::A.bits | Self::B.bits | Self::C.bits;
+    }
+}
+
 fn main() {
     // write to a vector or other io::Write
     let mut buf = Vec::new();
@@ -12,4 +24,11 @@ fn main() {
             println!("Converted number: {:?}", buf);
         }
     }
+
+    let e1 = Flags::A | Flags::C;
+    let e2 = Flags::B | Flags::C;
+    assert_eq!((e1 | e2), Flags::ABC);   // union
+    assert_eq!((e1 & e2), Flags::C);     // intersection
+    assert_eq!((e1 - e2), Flags::A);     // set difference
+    assert_eq!(!e2, Flags::A);           // set complement
 }
